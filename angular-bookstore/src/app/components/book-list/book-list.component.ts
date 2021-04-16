1d85752a94a1a6df6aed189e49bc2bc1c1fdd7c1
kasim basha <basha_788@yahoo.com>
2021-04-17 00:21:28 +0300

    Adding Angular changes

---
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { BookService } from 'src/app/services/book.service';
+import {Book} from '../../common/book';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+  books:Book [];
+  constructor(private bService : BookService) { }
+
+  ngOnInit(): void {
+    this.listBooks()
+  }
+
+  listBooks(){
+    this.bService.getBooks().subscribe(
+      data=> this.books = data
+    )
+  }
+}
--- onlinebookstore/src/main/java/in/kasimbasha/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/in/kasimbasha/onlinebookstore/repository/BookRepository.java
@@ -2,10 +2,11 @@ package in.kasimbasha.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import in.kasimbasha.onlinebookstore.entity.Book;
 
-
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
